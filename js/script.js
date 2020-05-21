0e3291f3aac90f9bae27c5590d3f8718a5dda8a2
Guilherme Soares <github@guilhermesoares.tech>
2020-05-21 20:56:11 -0300

    Iniciado o aruivo script.js e criado função de inserção
    Iniciado o aruivo script.js e criado função de inserção de nomes no html

---
+++ js/script.js
@@ -0,0 +1,61 @@
+window.addEventListener('load', start);
+
+var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
+var inputName = null;
+
+function start() {
+  inputName = document.querySelector('#inputName');
+  
+  preventFormSubmit();
+  activateInput();
+  render();
+}
+
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault(); // Evita que o formulário seja recarregado
+  }
+
+  var form = document.querySelector('form');
+  form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput() {
+  function insertName(newName) {
+    globalNames.push(newName);
+    render();
+  }
+
+  function handleTyping (event) {
+    if (event.key === 'Enter') {
+      insertName(event.target.value);
+    }
+  }
+
+  inputName.addEventListener('keyup', handleTyping)
+  inputName.focus();
+}
+
+function render() {
+  var divNames = document.querySelector('#names');
+  divNames.innerHTML = '';
+
+  var ul = document.createElement('ul');
+
+  for(var i = 0; i < globalNames.length; i++) {
+    var currentName = globalNames[i];
+
+    var li = document.createElement('li');
+    li.textContent = currentName;
+    ul.appendChild(li);
+  }
+
+  divNames.appendChild(ul);
+
+  clearInput();
+}
+
+function clearInput () {
+  inputName.value = '';
+  inputName.focus();
+}
