3e7877243fea92b0c80899570f12579cb41e0689
Guilherme Soares <github@guilhermesoares.tech>
2020-05-21 22:45:57 -0300

    Desenvolvido função de atualizar.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 window.addEventListener('load', start);
 
-var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
+var globalNames = ['Guilherme Soares Cota', 'Kenzawin Adliz Pereira'];
 var inputName = null;
+var currentIndex = null;
+var isEditing = false;
 
 function start() {
   inputName = document.querySelector('#inputName');
-  
+
   preventFormSubmit();
   activateInput();
   render();
@@ -23,12 +25,41 @@ function preventFormSubmit() {
 function activateInput() {
   function insertName(newName) {
     globalNames.push(newName);
-    render();
   }
 
-  function handleTyping (event) {
+  function updateName(newName) {
+    globalNames[currentIndex] = newName;
+  }
+
+  function handleTyping(event) {
+
+    var hasText = !!event.target.value && event.target.value.trim() !== ''; // Verificação, se o input estiver vazio torna a variavel false, senao true
+
+    if (!hasText) {      
+      if (event.key === 'Enter') {
+        M.toast({ html: 'Preencha um nome.' });
+      }
+      clearInput();
+      return;
+    }
+
     if (event.key === 'Enter') {
-      insertName(event.target.value);
+      if (event.target.value.length >= 6) {
+        if (isEditing) {
+          updateName(event.target.value);
+        }
+        else {
+          insertName(event.target.value);
+        }
+      }
+      else {
+        M.toast({ html: 'O neme deve conter no mínimo 6 letras.' })
+        return;
+      }
+
+      render();
+      isEditing = false;
+      clearInput();
     }
   }
 
@@ -40,7 +71,7 @@ function render() {
   function createDelletButton(index) {
     function deleteName() {
       globalNames.splice(index, 1); // Eclui o elemento do vetor na posição index, exclui apenas 1
-      
+      M.toast({ html: 'Nome excluído com sucesso!' });
       render();
     }
 
@@ -48,12 +79,28 @@ function render() {
     var iconDelete = document.createElement('i');
     iconDelete.classList.add('material-icons');
     iconDelete.textContent = 'delete';
-    button.classList.add('deleteButton');
+    button.classList.add('deleteButton', 'btn-floating', 'waves-effect', 'waves-light', 'red');
+    button.classList.add('z-depth-2');
     button.appendChild(iconDelete);
-
     button.addEventListener('click', deleteName);
 
     return button;
+  }
+
+  function crateSpan(name, index) {
+    function editItem() {
+      inputName.value = name;
+      inputName.focus();
+      isEditing = true;
+      currentIndex = index;
+    }
+
+    var span = document.createElement('span');
+    span.classList.add('clickable', 'flow-text');
+    span.textContent = name;
+    span.addEventListener('click', editItem);
+
+    return span;
   }
 
 
@@ -62,18 +109,17 @@ function render() {
 
   var ul = document.createElement('ul');
 
-  for(var i = 0; i < globalNames.length; i++) {
+  for (var i = 0; i < globalNames.length; i++) {
     var currentName = globalNames[i];
 
     var li = document.createElement('li');
-  
     var button = createDelletButton(i);
-    
-    var span = document.createElement('span');
-    span.textContent = currentName;
-    
+    var span = crateSpan(currentName, i);
+
+
     li.appendChild(button);
     li.appendChild(span);
+
     ul.appendChild(li);
   }
 
@@ -82,7 +128,7 @@ function render() {
   clearInput();
 }
 
-function clearInput () {
+function clearInput() {
   inputName.value = '';
   inputName.focus();
 }
